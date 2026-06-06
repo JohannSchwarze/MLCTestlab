59fce7d66fdb502e4e296e1731f8e6c6ffe444d1
Johann Schwarze <johann.schwarze@q-17.de>
2026-06-06 15:50:55 +0200

    fix testing bugs

--- einsum_trees/ops_tests.cpp
+++ einsum_trees/ops_tests.cpp
@@ -13,10 +13,10 @@
 
 
 
-double max_abs_diff(float const* in0, float const* in1, uint64_t const size) {
-    double result = 0.0;
+float max_abs_diff(float const* in0, float const* in1, uint64_t const size) {
+    float result = 0.0;
     for (uint64_t i = 0; i < size; ++i) {
-        double diff = abs(in0[i] - in1[i]);
+        float diff = abs(in0[i] - in1[i]);
         result = std::max(result, diff);
     }
     return result;
@@ -26,7 +26,7 @@ double max_abs_diff(float const* in0, float const* in1, uint64_t const size) {
 
 void load_tensor(float* out, uint64_t size, const char* fp) {
     std::ifstream file(fp, std::ios::binary);
-    file.read((char*) out, size);
+    file.read((char*) out, size * sizeof(float));
 }
 
 
@@ -51,7 +51,7 @@ TEST_CASE("test v1", "[test]") {
     opv1_bacd_cdef_abef(bacd.data(), cdef.data(), abef.data());
     opv1_abef_efgh_gabh(abef.data(), efgh.data(), gabh.data());
 
-    double diff = max_abs_diff(gabh.data(), gabh_exp.data(), gabh.size());
+    float diff = max_abs_diff(gabh.data(), gabh_exp.data(), gabh.size());
     REQUIRE(diff < 1e-5);
     */
 }
@@ -78,6 +78,78 @@ TEST_CASE("test transpose", "[test]") {
     REQUIRE(result);
 }
 
+TEST_CASE("test transpose 2", "[test]") {
+    transposefunction func(3, 3, 6, 6);
+
+    std::vector<float> in0 = {
+        1, 2, 3, 0, 0, 0,
+        4, 5, 6, 0, 0, 0,
+        7, 8, 9, 0, 0, 0,
+    };
+    std::vector<float> out(3 * 6, 0.0f);
+    std::vector<float> out_exp = {
+        1, 4, 7, 0, 0, 0,
+        2, 5, 8, 0, 0, 0,
+        3, 6, 9, 0, 0, 0,
+    };
+
+    func(in0.data(), out.data());
+
+    bool result = out == out_exp;
+    REQUIRE(result);
+}
+
+
+TEST_CASE("test bacd->cdab", "[test]") {
+    std::vector<float> bacd(db * da * dc * dd, 0.0f);
+    std::vector<float> cdab(dc * dd * da * db, 0.0f);
+    std::vector<float> cdab_exp(dc * dd * da * db, 0.0f);
+
+    load_tensor(bacd.data(), bacd.size(), "data/bacd_cdab/bacd.bin");
+    load_tensor(cdab_exp.data(), cdab_exp.size(), "data/bacd_cdab/cdab.bin");
+
+    opv2_bacd_cdab(bacd.data(), cdab.data());
+
+    float diff = max_abs_diff(cdab.data(), cdab_exp.data(), cdab.size());
+    REQUIRE(diff < 1e-6);
+}
+
+
+
+TEST_CASE("test cdab_cdef_efab", "[test]") {
+    std::vector<float> cdab(dc * dd * da * db, 0.0f);
+    std::vector<float> cdef(dc * dd * de * df, 0.0f);
+    std::vector<float> efab(de * df * da * db, 0.0f);
+    std::vector<float> efab_exp(de * df * da * db, 0.0f);
+
+    load_tensor(cdab.data(), cdab.size(), "data/cdab_cdef_efab/cdab.bin");
+    load_tensor(cdef.data(), cdef.size(), "data/cdab_cdef_efab/cdef.bin");
+    load_tensor(efab_exp.data(), efab_exp.size(), "data/cdab_cdef_efab/efab.bin");
+
+    opv2_cdab_cdef_efab(cdab.data(), cdef.data(), efab.data());
+
+    float diff = max_abs_diff(efab.data(), efab_exp.data(), efab.size());
+    REQUIRE(diff < 1e-6);
+}
+
+TEST_CASE("test efgh_efab_gabh", "[test]") {
+    std::vector<float> efgh(de * df * dg * dh, 0.0f);
+    std::vector<float> efab(de * df * da * db, 0.0f);
+    std::vector<float> gabh(dg * da * db * dh, 0.0f);
+    std::vector<float> gabh_exp(dg * da * db * dh, 0.0f);
+
+    load_tensor(efgh.data(), efgh.size(), "data/efgh_efab_gabh/efgh.bin");
+    load_tensor(efab.data(), efab.size(), "data/efgh_efab_gabh/efab.bin");
+    load_tensor(gabh_exp.data(), gabh_exp.size(), "data/efgh_efab_gabh/gabh.bin");
+
+    opv2_efgh_efab_gabh(efgh.data(), efab.data(), gabh.data());
+
+    float diff = max_abs_diff(gabh.data(), gabh_exp.data(), gabh.size());
+    REQUIRE(diff < 1e-6);
+}
+
+
+
 
 TEST_CASE("test v2", "[test]") {
     std::vector<float> bacd(db * da * dc * dd, 0.0f);
@@ -88,20 +160,17 @@ TEST_CASE("test v2", "[test]") {
     std::vector<float> gabh(dg * da * db * dh, 0.0f);
     std::vector<float> gabh_exp(dg * da * db * dh, 0.0f);
 
-    load_tensor(bacd.data(), bacd.size(), "data/bacd.bin");
-    load_tensor(cdef.data(), cdef.size(), "data/cdef.bin");
-    load_tensor(efgh.data(), efgh.size(), "data/efgh.bin");
-    load_tensor(gabh_exp.data(), gabh_exp.size(), "data/gabh.bin");
+    load_tensor(bacd.data(), bacd.size(), "data/main/bacd.bin");
+    load_tensor(cdef.data(), cdef.size(), "data/main/cdef.bin");
+    load_tensor(efgh.data(), efgh.size(), "data/main/efgh.bin");
+    load_tensor(gabh_exp.data(), gabh_exp.size(), "data/main/gabh.bin");
 
     opv2_bacd_cdab(bacd.data(), cdab.data());
     opv2_cdab_cdef_efab(cdab.data(), cdef.data(), efab.data());
     opv2_efgh_efab_gabh(efgh.data(), efab.data(), gabh.data());
 
-    std::cout << gabh[0] << std::endl;
-    std::cout << gabh_exp[0] << std::endl;
-
-    double diff = max_abs_diff(gabh.data(), gabh_exp.data(), gabh.size());
-    REQUIRE(diff < 1e-5);
+    float diff = max_abs_diff(gabh.data(), gabh_exp.data(), gabh.size());
+    REQUIRE(diff < 1e-6);
 }
 
 
--- einsum_trees/ref_ops.py
+++ einsum_trees/ref_ops.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from pathlib import Path
 
 
 da = 64
@@ -16,8 +16,51 @@ dh = 48
 
 
 
+def dump_bacd_cdab():
+    bacd = np.random.rand(db, da, dc, dd).astype(np.float32)
+    cdab = np.zeros((dc, dd, da, db), dtype=np.float32)
 
-def main():
+    np.einsum("bacd->cdab", bacd, out=cdab)
+    cdab = np.ascontiguousarray(cdab)
+
+    path = "data/bacd_cdab"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    bacd.reshape(-1).tofile(f"{path}/bacd.bin")
+    cdab.reshape(-1).tofile(f"{path}/cdab.bin")
+
+
+
+def dump_cdab_cdef_efab():
+    cdab = np.random.rand(dc, dd, da, db).astype(np.float32)
+    cdef = np.random.rand(dc, dd, de, df).astype(np.float32)
+    efab = np.zeros((de, df, da, db), dtype=np.float32)
+
+    np.einsum("cdab,cdef->efab", cdab, cdef, out=efab)
+    efab = np.ascontiguousarray(efab)
+
+    path = "data/cdab_cdef_efab"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    cdab.reshape(-1).tofile(f"{path}/cdab.bin")
+    cdef.reshape(-1).tofile(f"{path}/cdef.bin")
+    efab.reshape(-1).tofile(f"{path}/efab.bin")
+
+
+def dump_efgh_efab_gabh():
+    efgh = np.random.rand(de, df, dg, dh).astype(np.float32)
+    efab = np.random.rand(de, df, da, db).astype(np.float32)
+    gabh = np.zeros((dg, da, db, dh), dtype=np.float32)
+
+    np.einsum("efgh,efab->gabh", efgh, efab, out=gabh)
+    gabh = np.ascontiguousarray(gabh)
+
+    path = "data/efgh_efab_gabh"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    efgh.reshape(-1).tofile(f"{path}/efgh.bin")
+    efab.reshape(-1).tofile(f"{path}/efab.bin")
+    gabh.reshape(-1).tofile(f"{path}/gabh.bin")
+
+
+def dump_main():
     print("Allocating and initializing memory ...")
     bacd = np.random.rand(db, da, dc, dd).astype(np.float32)
     cdef = np.random.rand(dc, dd, de, df).astype(np.float32)
@@ -27,15 +70,19 @@ def main():
 
     print("Computing bacd,cdef->abef ...")
     np.einsum("bacd,cdef->abef", bacd, cdef, out=abef)
+    abef = np.ascontiguousarray(abef)
     print("Computing abef,efgh->gabh ...")
     np.einsum("abef,efgh->gabh", abef, efgh, out=gabh)
+    gabh = np.ascontiguousarray(gabh)
 
     print("Writing tensors to data directory ...")
-    bacd.reshape(-1).tofile("data/bacd.bin")
-    cdef.reshape(-1).tofile("data/cdef.bin")
-    efgh.reshape(-1).tofile("data/efgh.bin")
-    abef.reshape(-1).tofile("data/abef.bin")
-    gabh.reshape(-1).tofile("data/gabh.bin")
+    path = "data/main"
+    Path(path).mkdir(parents=True, exist_ok=True)
+    bacd.reshape(-1).tofile(f"{path}/bacd.bin")
+    cdef.reshape(-1).tofile(f"{path}/cdef.bin")
+    efgh.reshape(-1).tofile(f"{path}/efgh.bin")
+    abef.reshape(-1).tofile(f"{path}/abef.bin")
+    gabh.reshape(-1).tofile(f"{path}/gabh.bin")
 
     print("Done.")
 
@@ -54,7 +101,10 @@ def inspect(fp):
 
 
 if __name__ == "__main__":
-    main()
+    dump_bacd_cdab()
+    dump_cdab_cdef_efab()
+    dump_efgh_efab_gabh()
+    dump_main()
 
 
 
